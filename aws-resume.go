4c87e56de3569c98b3d50187a776c4d4ba22ddc4
michaelell <michael.ell@rearc.io>
2023-04-28 18:25:38 -0400

    init

---
+++ aws-resume.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type AwsResumeStackProps struct {
+	awscdk.StackProps
+}
+
+func NewAwsResumeStack(scope constructs.Construct, id string, props *AwsResumeStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewAwsResumeStack(app, "AwsResumeStack", &AwsResumeStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module aws-resume
+
+go 1.18
+
+require (
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.77.0
+	github.com/aws/constructs-go/constructs/v10 v10.1.270
+	github.com/aws/jsii-runtime-go v1.78.1
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/cdklabs/awscdk-asset-awscli-go/awscliv1/v2 v2.2.97 // indirect
+	github.com/cdklabs/awscdk-asset-kubectl-go/kubectlv20/v2 v2.1.1 // indirect
+	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv5/v2 v2.0.77 // indirect
+)
